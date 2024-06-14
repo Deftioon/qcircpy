9721157e93435e686a20f305f589db0c2d8faed3
Jake Lu <jylu1205@outlook.com>
2024-06-14 09:07:47 +0800

    z

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(name="Py-Q", 
+      version="1.0.0", 
+      description="Quantum Circuit Benchmarking", 
+      long_description=long_description, 
+      long_description_content_type="text/markdown", 
+      url="https://github.com/Deftioon/PyQ", 
+      packages=find_packages(), 
+      python_requires=">=3.9", )
